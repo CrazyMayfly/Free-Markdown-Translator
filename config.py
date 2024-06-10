c1c8d8b10a59b66877a7c6e12eb90d23b7e24808
xxnuo <bigtear@vip.qq.com>
2024-06-10 11:08:51 +0800

    更新翻译引擎
    将翻译引擎改为 https://github.com/UlionTse/translators 维护的 Google 翻译，经测试可正常使用

--- config.py
+++ config.py
@@ -5,29 +5,30 @@ insert_warnings = True
 src_language = "auto"
 # 配置目标语言及其warning，默认按照定义顺序翻译为下面语言
 warnings_mapping = {
-    'zh-tw': "警告：本文由機器翻譯生成，可能導致質量不佳或信息有誤，請謹慎閱讀！",
+    'zh': "警告：本文由机器翻译生成，可能导致质量不佳或信息有误，请谨慎阅读！",
+    # 'zh-TW': "警告：本文由機器翻譯生成，可能導致質量不佳或信息有誤，請謹慎閱讀！",
     'en': 'Warning: This page is translated by MACHINE, which may lead to POOR QUALITY or INCORRECT INFORMATION, please read with CAUTION!',
     'ja': '警告: この記事は機械翻訳されているため、品質が低かったり不正確な情報が含まれる可能性があります。よくお読みください。',
-    # 西班牙语
-    'es': 'Advertencia: este artículo está traducido por una máquina, lo que puede dar lugar a una mala calidad o información incorrecta. ¡Lea atentamente!',
-    # 俄语
+    # # 西班牙语
+    # 'es': 'Advertencia: este artículo está traducido por una máquina, lo que puede dar lugar a una mala calidad o información incorrecta. ¡Lea atentamente!',
+    # # 俄语
     'ru': 'Предупреждение: Эта статья переведена автоматически, что может привести к некачественной или неверной информации, пожалуйста, внимательно прочитайте!',
-    # 法语
-    'fr': 'Attention : cet article est traduit par machine, ce qui peut entraîner une mauvaise qualité ou des informations incorrectes, veuillez lire attentivement !',
-    # 德语
-    'de': 'Achtung: Dieser Artikel wurde maschinell übersetzt, was zu schlechter Qualität oder falschen Informationen führen kann, bitte sorgfältig lesen!',
-    # 印地语
-    'hi': 'चेतावनी: यह लेख मशीन द्वारा अनुवादित है, जिससे खराब गुणवत्ता या गलत जानकारी हो सकती है, कृपया ध्यान से पढ़ें!',
-    # 葡萄牙语
-    'pt': 'Aviso: Este artigo é traduzido por máquina, o que pode levar a má qualidade ou informações incorretas, leia com atenção!',
-    # 韩语
-    'ko': '경고: 이 기사는 기계 번역으로 생성되어 품질이 좋지 않거나 잘못된 정보로 이어질 수 있으므로 주의 깊게 읽으십시오!'
+    # # 法语
+    # 'fr': 'Attention : cet article est traduit par machine, ce qui peut entraîner une mauvaise qualité ou des informations incorrectes, veuillez lire attentivement !',
+    # # 德语
+    # 'de': 'Achtung: Dieser Artikel wurde maschinell übersetzt, was zu schlechter Qualität oder falschen Informationen führen kann, bitte sorgfältig lesen!',
+    # # 印地语
+    # 'hi': 'चेतावनी: यह लेख मशीन द्वारा अनुवादित है, जिससे खराब गुणवत्ता या गलत जानकारी हो सकती है, कृपया ध्यान से पढ़ें!',
+    # # 葡萄牙语
+    # 'pt': 'Aviso: Este artigo é traduzido por máquina, o que pode levar a má qualidade ou informações incorretas, leia com atenção!',
+    # # 韩语
+    # 'ko': '경고: 이 기사는 기계 번역으로 생성되어 품질이 좋지 않거나 잘못된 정보로 이어질 수 있으므로 주의 깊게 읽으십시오!'
 }
 # 指定目标语言
-# dest_langs = ['en','zh-tw']
+# dest_langs = ['en','zh-TW']
 dest_langs = warnings_mapping.keys()
 # 紧凑型语言，解决英语等非紧凑型语言的分隔问题
-compact_langs = ['zh-tw', 'ja']
+compact_langs = ['zh-TW', 'ja']
 # 指定要跳过翻译的字符的正则表达式，分别为加粗符号、在``中的非中文字符，`，用于过滤表格的符号，换行符
 skipped_regexs = [r"\*\*。?", r'#+', r'`[^\u4E00-\u9FFF]*?`', r'`', r'"[^\u4E00-\u9FFF]*?"', r'-+', r'\|', '\n']
 # 非紧凑型语言中需要添加分隔的正则表达式
